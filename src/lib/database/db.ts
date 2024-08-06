2d271a43f9a9b9b62f91cd0ade53ce9cc3380cfd
Marco Whyte <marcolwhyte@gmail.com>
2024-08-06 19:26:38 -0300

    revert

--- src/lib/database/db.ts
+++ src/lib/database/db.ts
@@ -1,24 +1,4 @@
-import { NeonDialect } from "kysely-neon";
-import { Kysely } from "kysely";
-import type { Database } from "./types";
-import { neonConfig } from "@neondatabase/serverless";
+import { createKysely } from '@vercel/postgres-kysely'
+import { Database } from './types'
 
-// if we're running locally
-if (!process.env.VERCEL_ENV) {
-  // Set the WebSocket proxy to work with the local instance
-  neonConfig.wsProxy = (host) => `${host}:5434/v1`;
-  // Disable all authentication and encryption
-  neonConfig.useSecureWebSocket = false;
-  neonConfig.pipelineTLS = false;
-  neonConfig.pipelineConnect = false;
-}
-
-if (!process.env.POSTGRES_URL) {
-  throw new Error("POSTGRES_URL is not set");
-}
-
-export const db = new Kysely<Database>({
-  dialect: new NeonDialect({
-    connectionString: process.env.POSTGRES_URL,
-  }),
-});
+export const db = createKysely<Database>({})
