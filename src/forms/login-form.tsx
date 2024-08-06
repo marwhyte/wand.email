3cf36c935f7233011084e58cb334d8edfafa1dc6
Marco Whyte <marcolwhyte@gmail.com>
2024-08-06 19:12:03 -0300

    try that again

--- src/forms/login-form.tsx
+++ src/forms/login-form.tsx
@@ -38,7 +38,7 @@ const GoogleLoginForm = () => {
 export default function LoginForm() {
   return (
     <>
-      <div className="relative h-full w-full rounded-xl bg-white px-6 py-12 shadow-[0px_0px_0px_1px_rgba(9,9,11,0.07),0px_2px_2px_0px_rgba(9,9,11,0.05)] dark:bg-zinc-900 dark:shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] dark:before:pointer-events-none dark:before:absolute dark:before:-inset-px dark:before:rounded-xl dark:before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
+      <div>
         <div className="mb-5">
           <Heading>Sign in to your account</Heading>
         </div>
