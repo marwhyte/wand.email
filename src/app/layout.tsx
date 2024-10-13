39b2161ef088a8b329ddc4b97187e2a9f32a1d9b
Marco Whyte <marcolwhyte@gmail.com>
2024-10-13 12:07:10 -0400

    fix middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { auth } from '@/auth'
 import type { Metadata } from 'next'
+import { SessionProvider } from 'next-auth/react'
 import { Inter } from 'next/font/google'
 import Content from './content'
 
@@ -28,13 +29,18 @@ export default async function RootLayout({
       className="text-zinc-950 antialiased dark:bg-zinc-900 dark:text-white lg:bg-zinc-100 dark:lg:bg-zinc-950"
     >
       <head>
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </head>
       <body className={inter.className + ' h-full'}>
-        {/* <SessionProvider session={session}> */}
-        <Content session={session}>{children}</Content>
-        {/* </SessionProvider> */}
+        <SessionProvider session={session}>
+          <Content session={session}>{children}</Content>
+        </SessionProvider>
       </body>
     </html>
   )
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,12 @@
-import NextAuth from 'next-auth'
-import { NextResponse } from 'next/server'
-
-import { authOptions } from './auth'
+import { auth } from '@/auth'
 import { PUBLIC_ROUTES, ROOT } from './lib/routes'
 
-const { auth } = NextAuth(authOptions)
-
-export async function middleware(request: any) {
-  const session = await auth()
-  const { nextUrl } = request
-
-  const isAuthenticated = !!session?.user?.email
-
-  const isPublicRoute = PUBLIC_ROUTES.find((route) => nextUrl.pathname.startsWith(route)) || nextUrl.pathname === ROOT
-
-  const isRoot = nextUrl.pathname === ROOT || nextUrl.pathname === ''
-
-  if (!isAuthenticated && !isPublicRoute) {
-    return NextResponse.redirect(new URL('/', nextUrl))
+export default auth((req) => {
+  if (!req.auth && !PUBLIC_ROUTES.includes(req.nextUrl.pathname) && req.nextUrl.pathname !== ROOT) {
+    const newUrl = new URL('/', req.nextUrl.origin)
+    return Response.redirect(newUrl)
   }
-}
-
+})
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
