e67b1adf389e59f67db7f53b5fa791cd449d7203
Marco Whyte <marcolwhyte@gmail.com>
2024-10-13 12:36:13 -0400

    fix auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,5 @@
-import { auth } from '@/auth'
-import { PUBLIC_ROUTES, ROOT } from './lib/routes'
+export { auth as middleware } from '@/auth'
 
-export default auth((req) => {
-  if (!req.auth && !PUBLIC_ROUTES.includes(req.nextUrl.pathname) && req.nextUrl.pathname !== ROOT) {
-    const newUrl = new URL('/', req.nextUrl.origin)
-    return Response.redirect(newUrl)
-  }
-})
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
