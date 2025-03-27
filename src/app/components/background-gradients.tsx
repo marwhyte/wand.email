66207762cae06692b1a7b0796cbfdf729032fab7
Marco Whyte <marcolwhyte@gmail.com>
2025-03-26 20:45:56 -0400

    some gradient on mobile

--- src/app/components/background-gradients.tsx
+++ src/app/components/background-gradients.tsx
@@ -85,7 +85,7 @@ export function BackgroundGradients({ inputDisabled }: Props) {
                 clipPath:
                   'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
               }}
-              className="relative left-[calc(60%+5rem)] top-[calc(15%)] aspect-[1155/678] w-[55rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] sm:w-[75rem]"
+              className="relative left-[calc(50%)] top-[calc(15%)] aspect-[1155/678] w-[36rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] sm:w-[75rem] md:left-[calc(60%+5rem)] md:w-[55rem]"
             />
           </motion.div>
 
@@ -106,7 +106,7 @@ export function BackgroundGradients({ inputDisabled }: Props) {
                 clipPath:
                   'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
               }}
-              className="relative left-[calc(90%+15rem)] top-[calc(50%)] aspect-[1155/678] w-[60rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] sm:w-[80rem]"
+              className="relative left-[calc(70%)] top-[calc(50%)] aspect-[1155/678] w-[40rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] sm:w-[80rem] md:left-[calc(90%+15rem)] md:w-[60rem]"
             />
           </motion.div>
 
@@ -127,7 +127,7 @@ export function BackgroundGradients({ inputDisabled }: Props) {
                 clipPath:
                   'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
               }}
-              className="relative left-[calc(95%-2rem)] top-[calc(5%)] aspect-[1155/678] w-[55rem] -translate-x-1/2 bg-gradient-to-tr from-[#9089fc] to-[#ff80b5] sm:w-[75rem]"
+              className="relative left-[calc(60%)] top-[calc(5%)] aspect-[1155/678] w-[36rem] -translate-x-1/2 bg-gradient-to-tr from-[#9089fc] to-[#ff80b5] sm:w-[75rem] md:left-[calc(95%-2rem)] md:w-[55rem]"
             />
           </motion.div>
         </>
--- src/app/components/dialogs/mobile-warning-dialog.tsx
+++ src/app/components/dialogs/mobile-warning-dialog.tsx
@@ -13,7 +13,7 @@ export function MobileWarningDialog({ isOpen, onClose, onConfirm }: Props) {
 
       <DialogTitle>Limited Functionality on Mobile</DialogTitle>
       <DialogDescription>
-        wand.email is best on larger screens. You can create, view, and test templates here, but for full editing
+        wand.email is best on larger screens. You can create, view, and test emails here, but for full editing
         functionality, we recommend using a computer.
       </DialogDescription>
       <DialogActions>
@@ -21,7 +21,7 @@ export function MobileWarningDialog({ isOpen, onClose, onConfirm }: Props) {
           Cancel
         </DialogButton>
         <DialogButton type="primary" onClick={onConfirm}>
-          Continue Anyway
+          Continue
         </DialogButton>
       </DialogActions>
     </Dialog>
