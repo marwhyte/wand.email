41aad5871b6318665f683417d7e4d599e5a65846
Marco Whyte <marcolwhyte@gmail.com>
2025-04-15 17:17:09 -0400

    another place

--- src/app/oauth-callback/[provider]/page.tsx
+++ src/app/oauth-callback/[provider]/page.tsx
@@ -2,7 +2,7 @@ import { OAuthProvider } from '@/lib/oauth/types'
 import OAuthCallbackClient from './client'
 
 // This is the Server Component (default in App Router)
-export default async function OAuthCallbackPage({ params }: { params: { provider: string } }) {
+export default async function OAuthCallbackPage({ params }: { params: Promise<{ provider: string }> }) {
   // In a server component, we can await the params
   const { provider } = await params
 
